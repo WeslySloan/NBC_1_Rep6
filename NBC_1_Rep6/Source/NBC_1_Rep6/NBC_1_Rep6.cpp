d456075f280d47a0ec4bc2786072bf863f6edc92
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-09-22 18:35:05 +0900

    (2025.09.22) 클래스 생성 및 프로젝트 셋업

---
+++ NBC_1_Rep6/Source/NBC_1_Rep6/NBC_1_Rep6.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_1_Rep6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_1_Rep6, "NBC_1_Rep6" );
